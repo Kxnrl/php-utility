a7c1c8b05a0afcbc9295ad08685c73a15ae21a47
Kyle <kyle@kxnrl.com>
2020-06-18 21:52:25 +0800

    Validate BossHp, entWatch, Translations

--- KeyValues/KeyValues.php
+++ KeyValues/KeyValues.php
@@ -2,23 +2,200 @@
 
 declare(strict_types=1);
 
-function errorHandler( int $errno , string $error) : bool {
+function errorHandler(int $errno , string $error) : bool {
 
     print_r("ERROR #" . $errno . " -> " . $error . PHP_EOL);
+
+    if ($errno != E_USER_NOTICE) {
+        print_r(json_encode(debug_backtrace(), JSON_PRETTY_PRINT) . PHP_EOL);
+    }
+
     exit(1);
+}
+
+function parseEntities(string $file, array $entities) : void {
+
+    if (!is_array($entities)) {
+        trigger_error("EntitiesParser: entities is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+        return;
+    }
+
+    global $fieldName;
+
+    foreach ($entities as $section => $entity) {
+
+        if (!is_array($entity)) {
+            trigger_error("EntitiesParser: key '$section' is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+            return;
+        }
+
+        // foreach key
+        $totalKeys = 0;
+        // check requires
+        foreach ($entity as $key => $val) {
+            if (in_array($key, $fieldName['entities']['require'], true)) {
+                $totalKeys++;
+            }
+        }
+        if (count($fieldName['entities']['require']) != $totalKeys) {
+            foreach ($fieldName['entities']['require'] as $key) {
+                if (!isset($entity[$key])) {
+                    trigger_error("EntitiesParser: missing filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                    return;
+                }
+            }
+        }
+
+        foreach ($entity as $key => $val) {
+            if (!in_array($key, $fieldName['entities']['optional'], true) && !in_array($key, $fieldName['entities']['require'], true)) {
+                trigger_error("EntitiesParser: redundant filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                return;
+            }
+        }
+    }
+}
+
+function parseBossHp(string $file, array $BossHp) : void {
+
+    if (!is_array($BossHp)) {
+        trigger_error("BossHpParser: entities is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+        return;
+    }
+
+    global $fieldName;
+
+    if (isset($BossHp['breakable'])) {
+
+        if (!is_array($BossHp['breakable'])) {
+            trigger_error("BossHpParser: breakable is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+            return;
+        }
+
+        foreach ($BossHp['breakable'] as $section => $breakable) {
+
+            if (!is_array($breakable)) {
+                trigger_error("BossHpParser: key '$section' is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                return;
+            }
+
+            // foreach key
+            $totalKeys = 0;
+            // check requires
+            foreach ($breakable as $key => $val) {
+                if (in_array($key, $fieldName['BossHP']['optional']['breakable']['require'], true)) {
+                    $totalKeys++;
+                }
+            }
+            if (count($fieldName['BossHP']['optional']['breakable']['require']) != $totalKeys) {
+                foreach ($fieldName['BossHP']['optional']['breakable']['require'] as $key) {
+                    if (!isset($breakable[$key])) {
+                        trigger_error("BossHpParser: missing filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                        return;
+                    }
+                }
+            }
+            foreach ($breakable as $key => $val) {
+                if (!in_array($key, $fieldName['BossHP']['optional']['breakable']['optional'], true) && !in_array($key, $fieldName['BossHP']['optional']['breakable']['require'], true)) {
+                    trigger_error("BossHpParser: redundant filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                    return;
+                }
+            }
+        }
+    }
+
+    if (isset($BossHp['counter'])) {
+
+        if (!is_array($BossHp['counter'])) {
+            trigger_error("BossHpParser: breakable is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+            return;
+        }
+
+        foreach ($BossHp['counter'] as $section => $counter) {
+
+            if (!is_array($counter)) {
+                trigger_error("BossHpParser: key '$section' is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                return;
+            }
+
+            // foreach key
+            $totalKeys = 0;
+            // check requires
+            foreach ($counter as $key => $val) {
+                if (in_array($key, $fieldName['BossHP']['optional']['counter']['require'], true)) {
+                    $totalKeys++;
+                }
+            }
+            if (count($fieldName['BossHP']['optional']['counter']['require']) != $totalKeys) {
+                foreach ($fieldName['BossHP']['optional']['counter']['require'] as $key) {
+                    if (!isset($counter[$key])) {
+                        trigger_error("BossHpParser: missing filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                        return;
+                    }
+                }
+            }
+            foreach ($counter as $key => $val) {
+                if (!in_array($key, $fieldName['BossHP']['optional']['counter']['optional'], true) && !in_array($key, $fieldName['BossHP']['optional']['counter']['require'], true)) {
+                    trigger_error("BossHpParser: redundant filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function parseTranslations(string $file, array $translations) : void {
+
+    if (!is_array($translations)) {
+        trigger_error("TranslationsParser: translations is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+        return;
+    }
+
+    global $fieldName;
+
+    foreach ($translations as $section => $tran) {
+
+        if (!is_array($tran)) {
+            trigger_error("TranslationsParser: key '$section' is not an array." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+            return;
+        }
+
+        // foreach key
+        $totalKeys = 0;
+        // check requires
+        foreach ($tran as $key => $val) {
+            if (in_array($key, $fieldName['Console_T']['require'], true)) {
+                $totalKeys++;
+            }
+        }
+        if (count($fieldName['Console_T']['require']) != $totalKeys) {
+            foreach ($fieldName['Console_T']['require'] as $key) {
+                if (!isset($tran[$key])) {
+                    trigger_error("TranslationsParser: missing filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                    return;
+                }
+            }
+        }
+        foreach ($tran as $key => $val) {
+            if (!in_array($key, $fieldName['Console_T']['optional'], true) && !in_array($key, $fieldName['Console_T']['require'], true)) {
+                trigger_error("TranslationsParser: redundant filed '$key' in '$section'." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                return;
+            }
+        }
+    }
 }
 
 // a simple parser for Valve's KeyValue format
 // https://developer.valvesoftware.com/wiki/KeyValues
 //
 // author: Rossen Popov, 2015-2016
-function vdf_decode(string $file) : array {
+function KvParser(string $file) : ?array
+{
 
     $text = file_get_contents($file);
 
     if(!is_string($text)) {
-        trigger_error("vdf_decode expects parameter 1 to be a string, " . gettype($text) . " given." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
-        return NULL;
+        trigger_error("KvParser expects parameter 1 to be a string, " . gettype($text) . " given." . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+        return null;
     }
 
     // detect and convert utf-16, utf-32 and convert to utf8
@@ -56,8 +233,8 @@ function vdf_decode(string $file) : array {
         }
 
         if($expect_bracket) {
-            trigger_error("vdf_decode: invalid syntax, expected a '}' on line " . ($i+1) . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
-            return Null;
+            trigger_error("KvParser: invalid syntax, expected a '}' on line " . ($i+1) . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+            return null;
         }
 
         // one level back
@@ -71,8 +248,8 @@ function vdf_decode(string $file) : array {
             preg_match($re_keyvalue, $line, $m);
 
             if(!$m) {
-                trigger_error("vdf_decode: invalid syntax on line " . ($i+1) . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
-                return NULL;
+                trigger_error("KvParser: invalid syntax on line " . ($i+1) . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+                return null;
             }
 
             $key = (isset($m['key']) && $m['key'] !== "")
@@ -104,8 +281,8 @@ function vdf_decode(string $file) : array {
     }
 
     if(count($stack) !== 1)  {
-        trigger_error("vdf_decode: open parentheses somewhere" . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
-        return NULL;
+        trigger_error("KvParser: open parentheses somewhere" . PHP_EOL . 'file: ' . $file, E_USER_NOTICE);
+        return null;
     }
 
     return $arr;
@@ -134,19 +311,70 @@ foreach ($DirsList as $dir) {
 
     foreach (scandir($dir) as $file) {
 
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
         $KeyValues[] = $dir . '/' . $file;
     }
 }
 
 $validated = 0;
+$fieldName = [
+    'entities' => [
+        'require' => [
+            'name', 'shortname', 'buttonclass', 'filtername', 'hasfiltername', 'hammerid', 'mode', 'glow', 'hud', 'autotransfer', 'maxuses', 'cooldown', 'maxamount'
+        ],
+        'optional' => [
+            'startcd', 'triggerid', 'isWall', 'level', 'children'
+        ]
+    ],
+    'Console_T' => [
+        'require' => [
+            'chi'
+        ],
+        'optional' => [
+            'command', 'blocked'
+        ]
+    ],
+    'BossHP' => [
+        'require' => [],
+        'optional' => [
+            'counter' => [
+                'require' => [
+                    'iterator'
+                ],
+                'optional' => [
+                    'backup', 'counter', 'displayname', 'countermode', 'multiparts'
+                ]
+            ],
+            'breakable' => [
+                'require' => [
+                    'targetname'
+                ],
+                'optional' => [
+                    'displayname', 'hpcounts', 'cashonly', 'multiparts'
+                ]
+            ]
+        ]
+    ]
+];
 foreach ($KeyValues as $kv) {
 
-    $array = vdf_decode(__DIR__ . '/' . $kv);
+    $local = (__DIR__ . '/' . $kv);
+    $array = KvParser($local);
+
     if (!is_array($array)) {
         trigger_error("Failed to decode file [$kv] -> is_array (Array) return false.", E_USER_NOTICE);
-    } else {
-        $validated++;
+        continue;
+    } else if (isset($array['entities'])) {
+        parseEntities($local, $array['entities']);
+    } else if (isset($array['Console_T'])) {
+        parseTranslations($local, $array['Console_T']);
+    } else if (isset($array['BossHP'])) {
+        parseBossHp($local, $array['BossHP']);
     }
+
+    $validated++;
 }
 
 print_r("Validated $validated / " . count($KeyValues) . " KeyValue files." . PHP_EOL);
